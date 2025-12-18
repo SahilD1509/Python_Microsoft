0f12e1e22aebdff0ee282d5d4c7b5e9d213173a7
SahilD1509 <sdhiman1509@gmail.com>
2025-12-18 19:13:10 +0530

    mc

---
+++ Basic/2Discount.py
@@ -0,0 +1,7 @@
+original_price = 75
+discount_rate = 15
+
+discount = (discount_rate/100) * original_price
+final_price = original_price - discount
+
+print("The final price after discount is: $", final_price)
---
+++ Basic/Discount.py
@@ -0,0 +1,4 @@
+discount  = 0.2
+price    = 100.0
+final_price = price * (1 - discount)
+print(final_price)
